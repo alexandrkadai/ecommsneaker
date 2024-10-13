6c192f5126c3259cbd2c5bc5e5dc11377b5c9c36
Alex Kadai <alexandrkadai@gmail.com>
2024-10-13 23:08:07 +0300

    Edit form ui finished

---
+++ app/components/dashboard/EditForm.tsx
@@ -0,0 +1,216 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Switch } from '@/components/ui/switch';
+import { Textarea } from '@/components/ui/textarea';
+import { ChevronLeft, XIcon } from 'lucide-react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { UploadDropzone } from '@/app/lib/uploadthing';
+import { SubmitButton } from '../SubmitButton';
+import { categories } from '@/app/lib/categories';
+import { useState } from 'react';
+import { useFormState } from 'react-dom';
+import { useForm } from '@conform-to/react';
+import { parseWithZod } from '@conform-to/zod';
+import { createProduct } from '@/app/actions';
+import { productSchema } from '@/app/lib/zodSchemas';
+import { type $Enums } from '@prisma/client';
+
+interface iAppProps {
+  data: {
+    images: string[];
+    name: string;
+    description: string;
+    status: $Enums.ProductStatus;
+    price: number;
+    category: $Enums.Category;
+    isFeatured: boolean;
+    id: string;
+    
+  };
+}
+
+export function EditForm({data}:iAppProps) {
+  const [images, setImages] = useState<string[]>(data.images);
+  const [lastResult, action] = useFormState(createProduct, undefined);
+  const [form, fields] = useForm({
+    lastResult,
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: productSchema });
+    },
+
+    shouldValidate: 'onBlur',
+    shouldRevalidate: 'onInput',
+  });
+
+  const handleDelete = (index: number) => {
+    setImages(images.filter((_, i) => i !== index));
+  };
+
+  return (
+    <form id={form.id} onSubmit={form.onSubmit} action={action}>
+      <div className="flex items-center gap-4">
+        <Button variant="outline" asChild>
+          <Link href="/dashboard/products">
+            <ChevronLeft size={24} />
+          </Link>
+        </Button>
+        <h1 className="text-xl font-semibold tracking-tight">New Product</h1>
+      </div>
+      <Card className="mt-5">
+        <CardHeader>
+          <CardTitle>Edit Product </CardTitle>
+          <CardDescription>In this form can edit your product</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-3">
+              <Label>Name</Label>
+              <Input
+                type="text"
+                key={fields.name.key}
+                name={fields.name.name}
+                defaultValue={data.name}
+                className="w-full"
+                placeholder="Product Name"
+              />
+              <p className="text-red-500">{fields.name.errors}</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <Label>Description</Label>
+              <Textarea
+                key={fields.description.key}
+                name={fields.description.name}
+                defaultValue={data.description}
+                placeholder="Write your Description ... "
+              />
+              <p className="text-red-500">{fields.description.errors}</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <Label>Price</Label>
+              <Input
+                key={fields.price.key}
+                name={fields.price.name}
+                defaultValue={data.price}
+                type="number"
+                placeholder="$444"
+              />
+              <p className="text-red-500">{fields.price.errors}</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <Label>Fetured Product</Label>
+              <Switch
+                key={fields.isFeatured.key}
+                name={fields.isFeatured.name}
+                checked={data.isFeatured}
+              />
+              <p className="text-red-500">{fields.isFeatured.errors}</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <Label>Status</Label>
+              <Select
+                key={fields.status.key}
+                name={fields.status.name}
+                defaultValue={data.status}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Status" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="draft">Draft</SelectItem>
+                  <SelectItem value="published">Published</SelectItem>
+                  <SelectItem value="archived">Archived</SelectItem>
+                </SelectContent>
+              </Select>
+              <p className="text-red-500">{fields.status.errors}</p>
+            </div>
+
+            <div className="flex flex-col gap-3">
+              <Label>Category</Label>
+              <Select
+                key={fields.category.key}
+                name={fields.category.name}
+                defaultValue={data.category}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Select Category ... " />
+                </SelectTrigger>
+                <SelectContent>
+                  {categories.map((item) => (
+                    <SelectItem key={item.id} value={item.name}>
+                      {item.title}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <p className="text-red-500">{fields.category.errors}</p>
+            </div>
+
+            <div className="flex flex-col gap-3">
+              <Label>Images</Label>
+              <input
+                type="hidden"
+                value={images}
+                key={fields.images.key}
+                name={fields.images.name}
+                defaultValue={data.images as any}
+              />
+              {images.length > 0 ? (
+                <div className="flex gap-5">
+                  {images.map((image, index) => (
+                    <div key={index} className="relative w-[100px] h-[100px]">
+                      <Image
+                        height={100}
+                        width={100}
+                        src={image}
+                        alt="uploaded image"
+                        className="w-full h-full rounded-lg border"
+                      />
+                      <button
+                        onClick={() => handleDelete(index)}
+                        type="button"
+                        className="absolute -top-3 -right-3 bg-red-500 p-2 rounded-lg text-white">
+                        <XIcon className="w-3 h-3" />
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <UploadDropzone
+                  className="ut-button:bg-purple-600"
+                  endpoint="imageUploader"
+                  onClientUploadComplete={(res) => {
+                    setImages(res.map((r) => r.url));
+                  }}
+                  onUploadError={() => {
+                    alert('Something Go Wrong');
+                  }}
+                />
+              )}
+              <p className="text-red-500">{fields.images.errors}</p>
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <SubmitButton />
+        </CardFooter>
+      </Card>
+    </form>
+  );
+}
--- app/dashboard/products/[id]/page.tsx
+++ app/dashboard/products/[id]/page.tsx
@@ -1,7 +1,23 @@
-export default function EditRoute() {
-    return (
-        <div className="">
-            <h1>hello from the edit route</h1>
-        </div>
-    )
+import { EditForm } from '@/app/components/dashboard/EditForm';
+import prisma from '@/app/lib/db';
+import { notFound } from 'next/navigation';
+
+async function getData(productId: string) {
+  const data = await prisma.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+  if (!data) {
+    return notFound();
+  }
+  return data;
+}
+
+export default async function EditRoute({ params }: { params: { id: string } }) {
+
+  const data = await getData(params.id);
+  return (
+   <EditForm data={data}/>
+  );
 }
